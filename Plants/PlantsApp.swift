4d40d0a48628ef4b650a18aa4fbd15486c26ec08
philparham <phil.parham@digital-tectonics.com>
2023-09-05 16:35:48 -0400

    Initial Commit

---
+++ Plants/PlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsApp.swift
+//  Plants
+//
+//  Created by Ramar Parham on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
